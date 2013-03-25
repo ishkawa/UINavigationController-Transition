a0f461c26fc934af9b0a9ffb4b8129cc74c6571e
xhzengAIB <xhzengAIB@gmail.com>
2013-03-25 18:19:26 +0000

    支持手势返回上一层的动作

--- DemoApp/DemoApp/UINavigationController+MyTransition.m
+++ DemoApp/DemoApp/UINavigationController+MyTransition.m
@@ -3,8 +3,51 @@
 
 @implementation UINavigationController (MyTransition)
 
+//这个是协议回调，所以需要遵循 UIGestureRecognizerDelegate协议
+-(void)handleSwipeFrom:(UISwipeGestureRecognizer *)recognizer {
+    if (recognizer == nil) return;
+    if (recognizer.direction==UISwipeGestureRecognizerDirectionDown) {
+        NSLog(@"向下滑动");
+        //执行程序
+    }
+    if (recognizer.direction==UISwipeGestureRecognizerDirectionUp) {
+        
+        NSLog(@"向上滑动");
+        //执行程序
+    }
+    if (recognizer.direction==UISwipeGestureRecognizerDirectionLeft) {
+        
+        NSLog(@"向左滑动");
+        //执行程序
+    }
+    if (recognizer.direction==UISwipeGestureRecognizerDirectionRight) {
+        
+        NSLog(@"向右滑动");
+        //执行程序
+        [self popViewControllerWithDuration:.7f
+                                 prelayouts:^(UIView *fromView, UIView *toView) {
+                                     toView.transform = CGAffineTransformMakeScale(.85, .85);
+                                 }
+                                 animations:^(UIView *fromView, UIView *toView) {
+                                     toView.transform = CGAffineTransformMakeScale(1, 1);
+                                     fromView.frame = CGRectMake(-fromView.frame.size.height,
+                                                                 0,
+                                                                 fromView.frame.size.width,
+                                                                 fromView.frame.size.height);
+                                 }
+                                 completion:nil];
+    }
+}
+
 - (void)customPushViewController:(UIViewController *)viewController
 {
+    UISwipeGestureRecognizer *recognizer;
+    
+    //向右滑动
+    recognizer = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeFrom:)];
+    [recognizer setDirection:(UISwipeGestureRecognizerDirectionRight)];
+    [[self view] addGestureRecognizer:recognizer];
+    [recognizer release];
     [self pushViewController:viewController
                     duration:.3f
                   prelayouts:^(UIView *fromView, UIView *toView) {
